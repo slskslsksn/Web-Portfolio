8f3de3dd429eba9912a1340efaf04eaec730aa9d
slskslsksn <slskslsksn@naver.com>
2023-04-06 20:36:48 +0900

    Migrating existing code to spring boot

---
+++ short-post/src/main/java/slskslsksn/web/shortpost/ShortPostApplication.java
@@ -0,0 +1,13 @@
+package slskslsksn.web.shortpost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortPostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortPostApplication.class, args);
+	}
+
+}
---
+++ short-post/src/test/java/slskslsksn/web/shortpost/ShortPostApplicationTests.java
@@ -0,0 +1,13 @@
+package slskslsksn.web.shortpost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortPostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
