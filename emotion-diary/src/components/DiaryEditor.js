783f67c66d8cb578f5308d9d4f2b4a4a79b78033
slskslsksn <slskslsksn@naver.com>
2023-08-15 20:06:57 +0900

    Optimization work

--- emotion-diary/src/components/DiaryEditor.js
+++ emotion-diary/src/components/DiaryEditor.js
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom';
-import { useContext, useEffect, useRef, useState } from 'react';
+import { useCallback, useContext, useEffect, useRef, useState } from 'react';
 import {DiaryDispatchContext} from "./../App.js";
 
 import MyButton from './MyButton';
@@ -19,9 +19,9 @@ const DiaryEditor = ({isEdit, originData}) => {
 
   const {onCreate, onEdit, onRemove} = useContext(DiaryDispatchContext);
 
-  const handleClickEmote = (emotion) => {
+  const handleClickEmote = useCallback((emotion) => {
     setEmotion(emotion);
-  };
+  }, []);
 
   const handleSubmit = () => {
     if(content.length < 1){
