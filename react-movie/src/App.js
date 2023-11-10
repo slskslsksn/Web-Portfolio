e31a3a06ff730b7966758581a4286e2f7007a58b
slskslsksn <slskslsksn@naver.com>
2023-11-10 14:05:41 +0900

    Move getMovieList from Home to App

--- react-movie/src/App.js
+++ react-movie/src/App.js
@@ -2,12 +2,23 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Detail from './pages/Detail';
 import './App.css';
+import { useEffect, useState } from 'react';
 function App() {
-  
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (await fetch('https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year')).json();
+    setMovies(json.data.movies);
+    console.log(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router basename={process.env.PUBLIC_URL} scrollBehavior="auto">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home loading={loading} movies={movies}/>} />
         <Route path="/detail/:id" element={<Detail />} />
       </Routes>
     </Router>
--- react-movie/src/pages/Home.js
+++ react-movie/src/pages/Home.js
@@ -5,18 +5,8 @@ import Loading from '../components/Loading';
 import styles from './Home.module.css';
 import Header from '../components/Header';
 
-function Home() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const json = await (await fetch('https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year')).json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-    console.log(movies);
-  }, []);
+const Home = ({loading, movies}) => {
+  
   return (
     <div className={styles.Home}>
       {loading ? (
